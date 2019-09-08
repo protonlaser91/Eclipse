568f211a41a0986d0f44f973362206200ffd3305
protonlaser91 <44213272+protonlaser91@users.noreply.github.com>
2019-09-08 10:37:42 -0400

    Updated config.ts
    removed public token

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export let config = {
-    "token": "MzMzNjg5MTY2NTQ0MTA5NTc5.XRUOhA.sV964o9DPyaUfhLHuzDgXEg2ndg",
+    "token": "INSERT OWN TOKEN HERE!",
     "prefix": "!",
     "commands": [
         "ascend",
